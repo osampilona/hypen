9e0d4fbfa2974a8746558fa9d6797a9829d293b6
osampilona <brzicamartina@gmail.com>
2024-02-15 10:44:41 +0100

    creating files

---
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import serviceCard from "@components/ServiceCard/serviceCard.module.scss";
+
+export interface IServiceCardProps {}
+
+const ServiceCard = (props: IServiceCardProps) => {
+  return <div>ServiceCard</div>;
+};
+
+export default ServiceCard;
