9d775cb17cbfc1a363cfcd657e3c1e061e65a160
osampilona <brzicamartina@gmail.com>
2024-03-09 14:07:54 +0100

    Creating servicedata.ts and connecting it to fetch static data (currently, fetch is not needed)

--- src/components/ServiceCardList/ServiceCardList.tsx
+++ src/components/ServiceCardList/ServiceCardList.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ServiceCard from "../ServiceCard/ServiceCard";
 import serviceCardList from "@/components/ServiceCardList/serviceCardList.module.scss";
+import { ServiceCardType } from "@/types/services/card";
+import { serviceData } from "@/data/serviceData";
 
-const ServiceCardList = () => {
-  const serviceCardArray = Array.from({ length: 8 }, (_, index) => index + 1);
+const ServiceCardList = async () => {
+  // const serviceData = Array.from({ length: 2 }, (_, index) => index + 1);
+  // const [serviceDataFromBackend, setServiceDataFromBackend] = useState<ServiceCardType[]>([]);
 
+  // // Call backend for data
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/api/serviceData")
+  //     .then((response) => response.json())
+  //     .then((data) => setServiceDataFromBackend(data))
+  // },[])
+  // }
+
+  // 3 states possible:
+  // 1. Loading
+  // 2. Error
+  // 3. Data
   return (
     <div className={serviceCardList.container}>
-      {serviceCardArray.map((_, index) => {
-        return <ServiceCard key={index} />;
-      })}
+      {serviceData.map((service: ServiceCardType, index: number) => (
+        <ServiceCard {...service} key={`${service.serviceName}-${index}`} />
+      ))}
     </div>
   );
 };
