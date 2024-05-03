c3d74283de6355c35140e19fe4282ad32ca6f306
osampilona <brzicamartina@gmail.com>
2024-05-03 07:56:33 +0200

    Adding two test cases

--- src/components/Skeletons/SkeletonCard/skeletonCard.test.tsx
+++ src/components/Skeletons/SkeletonCard/skeletonCard.test.tsx
@@ -1,10 +1,34 @@
-import "@testing-library/jest-dom";
+import React from "react";
 import { render, screen } from "@testing-library/react";
-import SkeletonCard from "./SkeletonCard";
+import SkeletonElement from "@/components/Skeletons/SkeletonCard/SkeletonCard";
+import skeleton from "@/components/Skeletons/SkeletonCard/skeletonCard.module.scss";
 
-describe("SkeletonCard", () => {
-  it("should render the SkeletonCard component", () => {
-    render(<SkeletonCard type={""} />);
-    expect(screen.getByTestId("skeleton-card")).toBeInTheDocument();
+describe("SkeletonElement", () => {
+  it("renders without crashing", () => {
+    render(<SkeletonElement type="card" />);
+    const skeletonElement = screen.getByTestId("skeleton-card");
+    expect(skeletonElement).toBeInTheDocument();
   });
-}); // Add closing parenthesis here
+
+  it("applies type-specific class based on the type prop", () => {
+    const { rerender } = render(<SkeletonElement type="card" />);
+    let skeletonElement = screen.getByTestId("skeleton-card");
+    expect(skeletonElement).toHaveClass(skeleton.skeleton);
+    expect(skeletonElement).toHaveClass(skeleton.card);
+
+    rerender(<SkeletonElement type="avatar" />);
+    skeletonElement = screen.getByTestId("skeleton-card");
+    expect(skeletonElement).toHaveClass(skeleton.skeleton);
+    expect(skeletonElement).toHaveClass(skeleton.avatar);
+  });
+
+  it("renders children when passed", () => {
+    const testMessage = "Loading your content...";
+    render(
+      <SkeletonElement type="card">
+        <div>{testMessage}</div>
+      </SkeletonElement>,
+    );
+    expect(screen.getByText(testMessage)).toBeInTheDocument();
+  });
+});
