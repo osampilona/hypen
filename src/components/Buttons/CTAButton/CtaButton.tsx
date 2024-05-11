5855fbf6cb9967fb2fb63e42e4fe726dd0625ac3
Martina Brzica <78907989+osampilona@users.noreply.github.com>
2024-05-11 16:42:36 +0200

    smaller fixes (#34)

--- src/components/Buttons/CTAButton/CtaButton.tsx
+++ src/components/Buttons/CTAButton/CtaButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import ctaButton from "@/ctaButton.module.scss";
+import ctaButton from "@/components/Buttons/CTAButton/ctaButton.module.scss";
 import { GoSearch } from "react-icons/go";
 
 export interface ICtaButtonProps {
