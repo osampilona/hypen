c2e1358e2948cf2dcaa43e58a44a5af16a1d2c7c
osampilona <brzicamartina@gmail.com>
2024-05-12 09:52:27 +0200

    Tests for Skeleton and shimmer

--- src/components/Skeletons/Shimmer/Shimmer.tsx
+++ src/components/Skeletons/Shimmer/Shimmer.tsx
@@ -3,8 +3,8 @@ import shimmer from "@/components/Skeletons/Shimmer/shimmer.module.scss";
 
 const Shimmer = () => {
   return (
-    <div className={shimmer.wrapper}>
-      <div className={shimmer.wrapper__shimmer}></div>
+    <div className={shimmer.wrapper} data-testid="shimmer-wrapper">
+      <div className={shimmer.wrapper__shimmer} data-testid="shimmer"></div>
     </div>
   );
 };
