5823268afb6dea69232d181e36a282405425d7e1
Martina Brzica <78907989+osampilona@users.noreply.github.com>
2024-05-15 22:40:23 +0200

    Navigation defined in the layout (#36)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import MenuBar from "@/components/MenuBar/MenuBar";
 import styles from "./page.module.css";
-import NavigationBar from "@/components/NavigationBar/NavigationBar";
 import ServiceCardList from "@/components/ServiceCardList/ServiceCardList";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <NavigationBar labelPartner="Become a partner" />
       <ServiceCardList />
       <MenuBar />
     </main>
