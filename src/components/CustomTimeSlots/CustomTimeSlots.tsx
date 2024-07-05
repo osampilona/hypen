e2f107e4ba045e2bdaa388179f54bda168e44c0c
osampilona <brzicamartina@gmail.com>
2024-07-05 18:06:26 +0200

    Time selection range styling

--- src/components/CustomTimeSlots/CustomTimeSlots.tsx
+++ src/components/CustomTimeSlots/CustomTimeSlots.tsx
@@ -7,7 +7,6 @@ import {
   setHours,
   setMinutes,
   isSameMinute,
-  isBefore,
 } from "date-fns";
 import CtaButton from "@/components/Buttons/CTAButton/CtaButton";
 
@@ -58,6 +57,68 @@ const CustomTimeSlots: React.FC<CustomTimeSlotsProps> = ({
     onStartSlotSelect(time);
   };
 
+  const calculateOverlayStyle = () => {
+    if (!startSlot || !endSlot) {
+      return { mainOverlay: {}, additionalOverlays: [] };
+    }
+
+    const startIndex = timeSlots.findIndex((time) =>
+      isSameMinute(time, startSlot),
+    );
+    const endIndex = timeSlots.findIndex((time) => isSameMinute(time, endSlot));
+
+    if (startIndex === -1 || endIndex === -1) {
+      return { mainOverlay: {}, additionalOverlays: [] };
+    }
+
+    const columnWidth = 100 / 4; // Assuming 4 columns
+    const rowHeight = 40; // Height of each time slot button
+    const margin = 2.5; // Margin around the buttons
+    const extraMargin = 3; // Extra margin to separate rows
+
+    const startColumn = startIndex % 4;
+    const endColumn = endIndex % 4;
+    const startRow = Math.floor(startIndex / 4);
+    const endRow = Math.floor(endIndex / 4);
+
+    const topOffset = startRow * (rowHeight + margin * 2) - margin;
+    const height = rowHeight + margin * 2;
+
+    let width = columnWidth * (4 - startColumn);
+    if (startRow === endRow) {
+      width = columnWidth * (endColumn - startColumn + 1);
+    }
+
+    const additionalOverlays: {
+      top: string;
+      left: string;
+      height: string;
+      width: string;
+    }[] = [];
+
+    if (startRow !== endRow) {
+      for (let row = startRow + 1; row <= endRow; row++) {
+        const isLastRow = row === endRow;
+        additionalOverlays.push({
+          top: `${row * (rowHeight + margin * 2.2) + (row - 1) * extraMargin}px`, // Adjusted top offset with extra margin
+          left: "calc(-2% + 2px)", // Added extra padding on the left side
+          height: `${rowHeight + margin * 2}px`,
+          width: `calc(${isLastRow ? (endColumn + 1) * columnWidth : 100}% + ${isLastRow ? 5 : 8}px)`, // Adjusted width calculation for all rows
+        });
+      }
+    }
+
+    return {
+      mainOverlay: {
+        top: `${topOffset}px`,
+        left: `calc(${startColumn * columnWidth}% - 3px)`, // Add padding to the left for the first row
+        height: `${height}px`,
+        width: `calc(${width}% + ${margin * 1}px + 5px)`, // Add a little more width for the first row
+      },
+      additionalOverlays,
+    };
+  };
+
   let timeSlots: Date[];
   switch (timeRange) {
     case "Morning":
@@ -73,29 +134,25 @@ const CustomTimeSlots: React.FC<CustomTimeSlotsProps> = ({
       timeSlots = [];
   }
 
+  const { mainOverlay, additionalOverlays } = calculateOverlayStyle();
+
   return (
     <div className={styles.container} data-testid="customTimeSlots">
-      {timeSlots.map((time, index) => {
-        const isSelected = selectedSlots.some((slot) =>
-          isSameMinute(slot, time),
-        );
-
-        const classNames = [isSelected ? styles.rangeSelected : ""]
-          .filter(Boolean)
-          .join(" ");
-
-        return (
-          <CtaButton
-            key={index}
-            label={format(time, "HH:mm")}
-            isPrimary={false}
-            outlined={true}
-            size="small"
-            onClick={() => handleSlotClick(time)}
-            className={classNames}
-          />
-        );
-      })}
+      <div className={styles.overlay} style={mainOverlay}></div>
+      {additionalOverlays.map((style, index) => (
+        <div key={index} className={styles.overlay} style={style}></div>
+      ))}
+      {timeSlots.map((time, index) => (
+        <CtaButton
+          key={index}
+          label={format(time, "HH:mm")}
+          isPrimary={false}
+          outlined={true}
+          size="small"
+          onClick={() => handleSlotClick(time)}
+          className={styles.timeSlot}
+        />
+      ))}
     </div>
   );
 };
