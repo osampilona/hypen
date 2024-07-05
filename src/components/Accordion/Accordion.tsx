cb195b71fb3d1c532432dd01b780b43ad854f8f1
Martina Brzica <78907989+osampilona@users.noreply.github.com>
2024-07-05 08:39:58 +0200

    Adjusting-time-slots-selection (#61)
    * Placing selected time slots next to the title, instead of single accordion.
    
    * Accross day options time selection
    
    * Reitterating time slots
    
    * Multiple accordions can be open
    
    * Fixing UI for time selected
    
    * Correct setup of choosing time from earlier to later
    
    * Last to earlier time slot selection
    
    * Fully function time selection

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -2,7 +2,14 @@ import accordion from "@/components/Accordion/accordion.module.scss";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { useState } from "react";
 import CustomTimeSlots from "@/components/CustomTimeSlots/CustomTimeSlots";
-import { format, addMinutes } from "date-fns";
+import {
+  format,
+  isBefore,
+  isSameMinute,
+  setHours,
+  setMinutes,
+  startOfDay,
+} from "date-fns";
 
 interface AccordionProps {
   title: string;
@@ -10,41 +17,69 @@ interface AccordionProps {
 }
 
 const Accordion: React.FC<AccordionProps> = ({ title, options }) => {
-  const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
-  const [selectedTimes, setSelectedTimes] = useState<(Date[] | null)[]>(
-    new Array(options.length).fill(null),
-  );
-  const [startSlot, setStartSlot] = useState<{
-    index: number;
-    time: Date | null;
-  }>({ index: -1, time: null });
+  const [expandedIndices, setExpandedIndices] = useState<number[]>([]);
+  const [startSlot, setStartSlot] = useState<Date | null>(null);
+  const [endSlot, setEndSlot] = useState<Date | null>(null);
+  const [isLastSelectedFirst, setIsLastSelectedFirst] =
+    useState<boolean>(false);
 
-  const handleClick = (index: number) => {
-    setExpandedIndex(expandedIndex === index ? null : index);
-    setStartSlot({ index: -1, time: null }); // Reset start slot when expanding/collapsing sections
+  const handleToggle = (index: number) => {
+    if (expandedIndices.includes(index)) {
+      setExpandedIndices(expandedIndices.filter((i) => i !== index));
+    } else {
+      setExpandedIndices([...expandedIndices, index]);
+    }
   };
 
-  const handleTimeSlotSelect = (times: Date[], index: number) => {
-    const newTimes = [...selectedTimes];
-    newTimes[index] = times;
-    setSelectedTimes(newTimes);
-    setStartSlot({ index: -1, time: null });
+  const handleTimeSlotSelect = (time: Date, isLastSlot: boolean) => {
+    if (isLastSlot && !startSlot) {
+      setStartSlot(null);
+      setEndSlot(time);
+      setIsLastSelectedFirst(true);
+    } else if (isLastSlot && startSlot) {
+      setEndSlot(time);
+      setIsLastSelectedFirst(false);
+    } else if (isLastSelectedFirst) {
+      setStartSlot(time);
+      setIsLastSelectedFirst(false);
+    } else if (!startSlot || (startSlot && endSlot)) {
+      setStartSlot(time);
+      setEndSlot(null);
+      setIsLastSelectedFirst(false);
+    } else if (startSlot && !endSlot) {
+      if (isBefore(startSlot, time)) {
+        setEndSlot(time);
+      } else {
+        setEndSlot(startSlot);
+        setStartSlot(time);
+      }
+      setIsLastSelectedFirst(false);
+    }
   };
 
-  const handleStartSlotSelect = (time: Date, index: number) => {
-    setStartSlot({ index, time });
-    const newTimes = [...selectedTimes];
-    newTimes[index] = [time]; // Set the new starting time
-    setSelectedTimes(newTimes);
-  };
-
-  const formatTimeRange = (times: Date[]) => {
-    if (times.length === 1) {
-      return `${format(times[0], "HH:mm")} - `;
-    } else if (times.length > 1) {
-      return `${format(times[0], "HH:mm")} to ${format(addMinutes(times[times.length - 1], 30), "HH:mm")}`;
+  const formatTimeRange = (
+    start: Date | null,
+    end: Date | null,
+    isLastSelectedFirst: boolean,
+  ) => {
+    if (start && end) {
+      return `${format(start, "HH:mm")} to ${format(end, "HH:mm")}`;
+    } else if (start) {
+      return `${format(start, "HH:mm")} - Choose end time`;
+    } else if (end && isLastSelectedFirst) {
+      return `Choose start time - ${format(setMinutes(setHours(startOfDay(end), 21), 0), "HH:mm")}`;
     }
     return "";
+  };
+
+  const isLastTimeSlot = (time: Date, timeRange: string): boolean => {
+    if (timeRange === "Evening") {
+      return isSameMinute(
+        time,
+        setHours(setMinutes(startOfDay(new Date()), 30), 21),
+      ); // 21:30 as the last slot
+    }
+    return false;
   };
 
   return (
@@ -55,42 +90,43 @@ const Accordion: React.FC<AccordionProps> = ({ title, options }) => {
       aria-labelledby="accordion"
     >
       <h3 className={accordion.title} id="accordion-title">
-        {title}
+        {title}{" "}
+        {startSlot || endSlot
+          ? ` - ${formatTimeRange(startSlot, endSlot, isLastSelectedFirst)}`
+          : ""}
       </h3>
       <ul className={accordion.list}>
         {options.map((option, index) => (
           <li className={accordion.item} key={index}>
             <button
               className={accordion.button}
-              aria-expanded={expandedIndex === index}
-              onClick={() => handleClick(index)}
+              aria-expanded={expandedIndices.includes(index)}
+              onClick={() => handleToggle(index)}
             >
-              <span className={accordion.text}>
-                {option}{" "}
-                {selectedTimes[index] && selectedTimes[index]?.length > 0
-                  ? ` - ${formatTimeRange(selectedTimes[index])}`
-                  : startSlot.index === index && startSlot.time
-                    ? `${format(startSlot.time, "HH:mm")} - `
-                    : ""}
-              </span>
+              <span className={accordion.text}>{option}</span>
               <span className={accordion.icon} aria-hidden="true">
-                {expandedIndex === index ? (
+                {expandedIndices.includes(index) ? (
                   <IoIosArrowUp />
                 ) : (
                   <IoIosArrowDown />
                 )}
               </span>
             </button>
-            {expandedIndex === index && (
+            {expandedIndices.includes(index) && (
               <div className={accordion.content}>
                 <CustomTimeSlots
                   timeRange={option}
                   onTimeSlotSelect={(times) =>
-                    handleTimeSlotSelect(times, index)
+                    handleTimeSlotSelect(
+                      times[times.length - 1],
+                      isLastTimeSlot(times[times.length - 1], option),
+                    )
                   }
                   onStartSlotSelect={(time) =>
-                    handleStartSlotSelect(time, index)
+                    handleTimeSlotSelect(time, isLastTimeSlot(time, option))
                   }
+                  startSlot={startSlot}
+                  endSlot={endSlot}
                 />
               </div>
             )}
