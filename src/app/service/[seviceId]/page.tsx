4efa0966a24c3ad488d2dcfb06169f7702779bc5
osampilona <brzicamartina@gmail.com>
2025-07-23 16:35:44 +0200

    small fix

--- src/app/service/[seviceId]/page.tsx
+++ src/app/service/[seviceId]/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Accordion from "@/components/Accordion/Accordion";
 import TimeSlotSelector from "@/components/TimeSlotsSelector/TimeSlotsSelector";
 const ServicePage = () => {
